7e85232e022862ec977bd66640066200bd565ddb
lokialice <lokialicehd@gmail.com>
2015-05-29 15:30:38 +0700

    Initial Commit

---
+++ ronDeleteRecorderTv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ronDeleteRecorderTv
+//
+//  Created by Macbook Pro MD102 on 5/29/15.
+//  Copyright (c) 2015 Macbook Pro MD102. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
