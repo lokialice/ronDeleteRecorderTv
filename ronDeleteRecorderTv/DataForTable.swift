35341f4beb7df3d81ada82dda7a167bec56a045e
lokialice <lokialicehd@gmail.com>
2015-07-09 23:01:42 +0700

    Table View Delete and Order Recorder

---
+++ ronDeleteRecorderTv/DataForTable.swift
@@ -0,0 +1,15 @@
+//
+//  DataForTable.swift
+//  ronDeleteRecorderTv
+//
+//  Created by Macbook Pro MD102 on 5/29/15.
+//  Copyright (c) 2015 Macbook Pro MD102. All rights reserved.
+//
+
+import Foundation
+
+let playersData = [ Player(name:"Larry Bird", position:"Forward"),
+    Player(name: "Robert Parish", position: "Center"),
+    Player(name: "Kevin McHale", position: "Forward"),
+    Player(name:"Danny Ainge", position:"Guard"),
+    Player(name:"Dennis Johnson", position:"Guard")]
---
+++ ronDeleteRecorderTv/Player.swift
@@ -0,0 +1,19 @@
+//
+//  Player.swift
+//  ronDeleteRecorderTv
+//
+//  Created by Macbook Pro MD102 on 5/29/15.
+//  Copyright (c) 2015 Macbook Pro MD102. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    var name:String
+    var position:String
+    
+    init(name:String,position:String){
+        self.name = name
+        self.position = position
+    }
+}
